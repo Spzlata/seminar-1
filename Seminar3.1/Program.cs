b519a247b0c24d73cf4792d3859f3fde1004718b
Spzlata <107999131+Spzlata@users.noreply.github.com>
2022-07-13 21:18:13 +0300

    master

--- Seminar3.1/Program.cs
+++ Seminar3.1/Program.cs
@@ -1,1 +1,26 @@
-﻿ //Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+ //Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+ void quarter(int A, int B);
+{
+    if (A > 0 && B > 0)
+    {
+        Console.WriteLine("First quarter");
+    }
+    else if (A < 0 && B > 0)
+    {
+        Console.WriteLine("Second quarter");
+    }
+    else if (A > 0 && B < 0)
+    {
+        Console.WriteLine("Third quarter");
+    }
+    else if (A < 0 && B < 0)
+    {
+        Console.WriteLine("Forth quarter");
+    }
+}
+
+quarter(5, 6);
+quarter(-5, 6);
+quarter(-5, -5);
+quarter(5, -6);
+
